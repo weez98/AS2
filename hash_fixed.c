0405d26e39781141f176a20ee0f9b9ad4ff60209
weez98 <96293490+weez98@users.noreply.github.com>
2023-02-08 23:41:06 +0300

    Update hash_fixed.c

--- hash_fixed.c
+++ hash_fixed.c
@@ -1,62 +1,134 @@
-#include <stddef.h>
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include "hash_fixed.h"
 
-#define MAP_MAX 256
+
 
 int main(){
+
 unsigned HashIndex(const char* key) {
+
     unsigned sum = 0;
-    for (const char* c = key; *c != '\0'; c++) {
+
+    // correct the for loop condition
+
+    for (const char* c = key; *c; c++){
+
         sum += *c;
+
     }
+
     
+
     return sum;
+
 }
+
+
 
 HashMap* HashInit() {
-    return malloc(sizeof(HashMap));
+
+    // initialize the data field
+
+    HashMap *map = malloc(sizeof(HashMap));
+
+    memset(map, 0, sizeof(HashMap));
+
+    return map;
+
 }
 
-void HashAdd(HashMap *map, PairValue *value) {
+
+
+void HashAdd(HashMap *map,PairValue *value) {
+
     unsigned idx = HashIndex(value->KeyName);
+
     
+
+    // move the assignment of value->Next to before the assignment of map->data[idx]
+
     if (map->data[idx]) 
-        value->Next = map->data[idx]->Next;
-    map->data[idx] = value;     
+
+        value->Next = map->data[idx];
+
+    map->data[idx] = value;  
+
 }
+
+
 
 PairValue* HashFind(HashMap *map, const char* key) {
+
     unsigned idx = HashIndex(key);
+
     
-    for(PairValue* val = map->data[idx]; val != NULL; val = val->Next) {
+
+    for( PairValue* val = map->data[idx]; val != NULL; val = val->Next ) {
+
+        // replace strcpy with strcmp to compare strings
+
         if (strcmp(val->KeyName, key) == 0)
+
             return val;
+
     }
+
     
+
     return NULL; 
+
 }
 
 void HashDelete(HashMap *map, const char* key) {
+
     unsigned idx = HashIndex(key);
+
     
-    for(PairValue* val = map->data[idx], *prev = NULL; val != NULL; prev = val, val = val->Next) {
+
+    for( PairValue* val = map->data[idx], *prev = NULL; val != NULL; prev = val, val = val->Next ) {
+
+        // replace strcpy with strcmp to compare strings
+
         if (strcmp(val->KeyName, key) == 0) {
+
             if (prev)
+
                 prev->Next = val->Next;
+
             else
+
                 map->data[idx] = val->Next;
+
         }
+
     }
+
 }
 
+
+
 void HashDump(HashMap *map) {
-    for(unsigned i = 0; i < MAP_MAX; i++) {
-        for(PairValue* val = map->data[i]; val != NULL; val = val->Next) {
-            printf("%s\n", val->KeyName);
+
+    for( unsigned i = 0; i < MAP_MAX; i++ ) {
+
+        for( PairValue* val = map->data[i]; val != NULL; val = val->Next ) {
+
+            printf("%s\n", val->KeyName); // added format specifier for printf
+
         }
+
     }
+
 }
+
 }
+
+
+
+
+
