cac4fb98193735d30652ec98a53a295066e3dc29
weez98 <96293490+weez98@users.noreply.github.com>
2023-02-08 23:22:01 +0300

    Update hash_fixed.h

--- hash_fixed.h
+++ hash_fixed.h
@@ -1,51 +1,48 @@
-/
- * @Name : hash.h
- *
- * Fix: Header comment added
-/
+#ifndef __HASH__
 
-#ifndef HASH
-#define HASH
+#define __HASH__
 
-/
- * Fix: Moved struct PairValue definition to after its usage
-/
-typedef struct PairValue PairValue;
-struct PairValue {
-    #define KEY_STRING_MAX 255
-    char KeyName[KEY_STRING_MAX];
-    int  ValueCount;
-    PairValue* Next;
-};
 
-typedef struct {
-    #define MAP_MAX 128
-    PairValue* data[MAP_MAX];
-} HashMap;
 
-/
- * Fix: Added argument types to HashInit prototype
-/
-HashMap* HashInit(void);
+    typedef struct PairValue {
 
-/
- * Fix: Added argument types to HashAdd prototype
-/
-void HashAdd(HashMap *map, PairValue *value);
+        #define KEY_STRING_MAX 255
 
-/
- * Fix: Added argument types to HashDelete prototype
-/
-void HashDelete(HashMap *map, const char* key);
+                char KeyName[KEY_STRING_MAX];
 
-/
- * Fix: Added argument types to HashFind prototype
-/
-PairValue* HashFind(HashMap *map, const char* key);
+                int  ValueCount;
 
-/
- * Fix: Added argument types to HashDump prototype
-/
-void HashDump(HashMap *map);
+        struct PairValue* Next;
+
+        } PairValue;
+
+
+
+        typedef struct {
+
+                int size;
+
+                int count;
+
+                PairValue** data;
+
+        } HashMap;
+
+
+
+    HashMap* HashInit(int size);
+
+    int HashAdd(HashMap *map, const char* key, int value);
+
+    void HashDelete(HashMap *map, const char* key);
+
+    PairValue* HashFind(HashMap *map, const char* key);
+
+    void HashDump(HashMap *map);
+
+    int HashFunction(const char* key, int size);
+
+    void HashDestroy(HashMap *map);
 
 #endif
+
