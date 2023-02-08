9fa82329443ab3ea803b05ab24e3aaf8c11b2c63
weez98 <96293490+weez98@users.noreply.github.com>
2023-02-08 22:44:50 +0300

    Update hash_fixed.h

--- hash_fixed.h
+++ hash_fixed.h
@@ -6,7 +6,7 @@
 #ifndef __HASH__
 #define __HASH__
 
-    typedef struct {
+    typedef struct PairValue {
         #define KEY_STRING_MAX 255
 		char KeyName[KEY_STRING_MAX];
 		int  ValueCount;
@@ -14,13 +14,16 @@
 	} PairValue;
 
 	typedef struct {
-        #define MAP_MAX 128
-		PairValue* data[MAP_MAX];
+		int size;
+		int count;
+		PairValue** data;
 	} HashMap;
 
-    HashMap* HashInit();
-    void HashAdd(HashMap *map, PairValue *value);
+    HashMap* HashInit(int size);
+    int HashAdd(HashMap *map, const char* key, int value);
     void HashDelete(HashMap *map, const char* key);
     PairValue* HashFind(HashMap *map, const char* key);
     void HashDump(HashMap *map);
+    int HashFunction(const char* key, int size);
+    void HashDestroy(HashMap *map);
 #endif
