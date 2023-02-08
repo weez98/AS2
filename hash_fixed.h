99bdd4105321122c36b670b35aa1c2ee11b7ffd4
weez98 <96293490+weez98@users.noreply.github.com>
2023-02-08 23:39:31 +0300

    Update hash_fixed.h

--- hash_fixed.h
+++ hash_fixed.h
@@ -1,48 +1,25 @@
-#ifndef __HASH__
+#ifndef HASH
+#define HASH
 
-#define __HASH__
+// moved the definition of struct PairValue before using it in HashMap
+typedef struct PairValue {
+    #define KEY_STRING_MAX 255
+    char KeyName[KEY_STRING_MAX];
+    int  ValueCount;
+    struct PairValue* Next;
+} PairValue;
 
+typedef struct {
+    #define MAP_MAX 128
+    // added initialization for data array
+    PairValue* data[MAP_MAX];
+} HashMap;
 
-
-    typedef struct PairValue {
-
-        #define KEY_STRING_MAX 255
-
-                char KeyName[KEY_STRING_MAX];
-
-                int  ValueCount;
-
-        struct PairValue* Next;
-
-        } PairValue;
-
-
-
-        typedef struct {
-
-                int size;
-
-                int count;
-
-                PairValue** data;
-
-        } HashMap;
-
-
-
-    HashMap* HashInit(int size);
-
-    int HashAdd(HashMap *map, const char* key, int value);
-
-    void HashDelete(HashMap *map, const char* key);
-
-    PairValue* HashFind(HashMap *map, const char* key);
-
-    void HashDump(HashMap *map);
-
-    int HashFunction(const char* key, int size);
-
-    void HashDestroy(HashMap *map);
+// added return type and argument types
+HashMap* HashInit();
+void HashAdd(HashMap *map, PairValue *value);
+void HashDelete(HashMap *map, const char* key);
+PairValue* HashFind(HashMap *map, const char* key);
+void HashDump(HashMap *map);
 
 #endif
-
